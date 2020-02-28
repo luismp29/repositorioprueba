25b2a498c4250b7d9086654a833958d1263c1634
luismp29 <60668091+luismp29@users.noreply.github.com>
2020-02-28 18:08:20 +0100

    subida ejercicio avanzado

---
+++ PrimerProyecto/src/paquete/HolaClase.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class HolaClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("fumate un pey ;)");//Esto para escribir
+
+	}
+
+}
